1514f7c80dd9da4012e353317accb8147f5e038c
Jugraj <jugrajdeol.power@gmail.com>
2023-04-12 20:34:39 -0500

    changed some css, need to learn more tailwind

---
+++ lib/information.tsx
@@ -0,0 +1,18 @@
+export const name = "Jugraj Deol"
+export const about = () => {
+    return(
+        <>
+            Hey, I`m Jugraj Deol, a <b>Software Developer</b> based in Canada where I work on making 
+            different systems talk to eachother using various tools.
+        </>
+    );
+};
+
+export const bio = () => {
+    return (
+    <>
+        I`m passionate about Backend and Integration development.I have experience doing customizations
+        in a Cloud Based ERP like Acumatica using C#.
+    </>
+    );
+};
